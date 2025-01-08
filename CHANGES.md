87225eaee41195931dd54fcb3c932b19b5104107
Калинкин Павел <pnzazfof@gmail.com>
2025-01-08 20:26:13 +0300

    fix excrescent recreates of file

--- CHANGES.md
+++ CHANGES.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [ZeroVer](https://0ver.org/).
 
+## [0.0.2] - 08-01-2024
+
+### Fixed
+- file no more recreates before every call - if it exists, we work with it
+
 ## [0.0.1] - 08-01-2024
 
 Initial release
--- Faxecutor.py
+++ Faxecutor.py
@@ -17,12 +17,16 @@ class Settings:
 
 class FaxecutorCommand(sublime_plugin.TextCommand):
     def _save_if_needed(self):
-        if self.view.is_dirty():
+        if self.view.file_name() and not self.view.is_dirty():
+            return
+
+        if not self.view.file_name():
             with tempfile.NamedTemporaryFile(
                 delete=False, suffix=self.settings.suffix, mode="w"
             ) as tmp_file:
                 self.view.retarget(tmp_file.name)
-                self.view.run_command("save")
+
+        self.view.run_command("save")
 
     def _show_output(self, message):
         output = self.view.window().create_output_panel("faxecutor_result")
